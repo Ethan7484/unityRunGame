73f64b25ed6e43cb3bb6a12ada5c03b89c9c5ffc
ethan7484 <sinji107@gmail.com>
2025-02-03 21:02:26 +0900

    노마드코더 김치런(무한 달리기)
    Player Jump. 완료 (58:30)

--- Infinity-Run/Assets/@Scripts/Player.cs
+++ Infinity-Run/Assets/@Scripts/Player.cs
@@ -7,6 +7,25 @@ public class Player : MonoBehaviour
     
     [Header("References")]
     public Rigidbody2D playerRb;
-    
-    
+
+    private bool isGrounded = true;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            playerRb.AddForceY(jumpForce, ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Platform")
+        {
+            isGrounded = true;
+        }
+    }
+
+
 }
